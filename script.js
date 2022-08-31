326824a0618b8b264f05950997836bec6b22761a
Andras Varga <zoryandras@gmail.com>
2022-08-31 17:06:38 +0200

    Just for practice.

---
+++ script.js
@@ -0,0 +1,16 @@
+let countEl = document.getElementById("count")
+let saveEl = document.getElementById("save")
+
+let count = 0;
+
+function  increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
